d40e43ba279c92b83410b40847d287c1837fbd58
Pete 'Peteches' McCabe <pete@peteches.co.uk>
2025-09-04 23:15:17 +0100

    update desktop service to use proper pipewire service

--- peteches/home-services/desktop.scm
+++ peteches/home-services/desktop.scm
@@ -1,75 +1,76 @@
 (define-module (peteches home-services desktop)
+  ;; your modules
   #:use-module (peteches packages fonts)
   #:use-module (peteches packages scripts)
+  #:use-module (peteches home-services wofi)
+  ;; guix home/service modules
   #:use-module (gnu home services)
   #:use-module (gnu home services shepherd)
-  #:use-module (gnu packages aidc)
-  #:use-module (gnu packages xdisorg)
-  #:use-module (gnu packages rust-apps)
-  #:use-module (gnu packages linux)
-  #:use-module (gnu packages version-control)
-  #:use-module (gnu packages wm)
+  #:use-module (gnu home services sound)      ; home-pipewire-service-type
+  ;; packages
+  #:use-module (gnu packages glib)            ; provides 'dbus' package
   #:use-module (gnu packages video)
+  #:use-module (gnu packages wm)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages xdisorg)
+  #:use-module (gnu packages version-control)
+  #:use-module (gnu packages rust-apps)
   #:use-module (gnu services)
   #:use-module (gnu services shepherd)
-  #:use-module (gnu packages )
   #:use-module (guix gexp)
-  #:use-module (peteches home-services wofi)
   #:export (home-desktop-service-type))
 
-(define (home-desktop-profile-service config)
+;; 1) Desktop packages as before
+(define (home-desktop-profile-service _config)
+  (list shell-scripts helvum dbus))   ; ensure dbus binary is in profile
+
+;; 2) User D-Bus session bus as a Shepherd service.
+;;    We provision it as 'dbus' because home-pipewire requires that.
+(define (home-desktop-shepherd-services _config)
   (list
-   shell-scripts
-   helvum
-   ;; Generically useful packages
-))
-
-(define (home-desktop-shepherd-service-type config)
-   (list
-   ;; PipeWire daemon
    (shepherd-service
-    (provision '(pipewire))
-    (documentation "User PipeWire daemon.")
-    (start #~(make-forkexec-constructor
-	      (list #$(file-append pipewire "/bin/pipewire"))
-              ;; optional log location:
-              #:log-file (string-append (or (getenv "XDG_STATE_HOME")
-                                            (string-append (getenv "HOME")
-                                                           "/.local/state"))
-                                        "/shepherd/pipewire.log")))
-    (stop  #~(make-kill-destructor))
-    (respawn? #t))
+     (provision '(dbus))
+     (documentation "User D-Bus session bus")
+     (requirement '()) ; none
+     (start
+      #~(make-forkexec-constructor
+         (list #$(file-append dbus "/bin/dbus-daemon")
+               "--session"              ; session bus (reads system config)
+               "--nofork"               ; let shepherd supervise it
+               "--print-address")       ; helpful for logs
+         ;; Make sure $XDG_RUNTIME_DIR exists; dbus defaults to $XDG_RUNTIME_DIR/bus.
+         #:environment-variables
+         (let* ((uid   (number->string (getuid)))
+                (xr    (or (getenv "XDG_RUNTIME_DIR")
+                           (string-append "/run/user/" uid))))
+           (list (string-append "XDG_RUNTIME_DIR=" xr)
+                 "DBUS_SESSION_BUS_ADDRESS="))) ; let libdbus default to $XDG_RUNTIME_DIR/bus
+     )
+     (stop #~(make-kill-destructor))
+     (respawn? #t))))
 
-   ;; PulseAudio compatibility (so PA apps see a 'pulseaudio' server)
-   (shepherd-service
-    (provision '(pipewire-pulse))
-    (requirement '(pipewire))
-    (documentation "PipeWire PulseAudio compatibility server.")
-    (start #~(make-forkexec-constructor
-	      (list #$(file-append pipewire "/bin/pipewire-pulse"))))
-    (stop  #~(make-kill-destructor))
-    (respawn? #t))
+;; 3) PipeWire (with Pulse shim) through Home’s sound service.
+(define (home-desktop-pipewire-service _config)
+  (home-pipewire-configuration
+    (enable-pulseaudio? #t)))
 
-   ;; WirePlumber session manager
-   (shepherd-service
-    (provision '(wireplumber))
-    (requirement '(pipewire))   ; start after pipewire socket/daemon
-    (documentation "WirePlumber session manager.")
-    (start #~(make-forkexec-constructor
-	      (list #$(file-append wireplumber "/bin/wireplumber"))))
-    (stop  #~(make-kill-destructor))
-    (respawn? #t))))
-
-
+;; 4) The desktop meta-service ties it all together.
 (define home-desktop-service-type
-  (service-type (name 'home-desktop)
-                (description "My desktop environment service.")
-                (extensions
-                 (list
-		  (service-extension
-		   home-shepherd-service-type
-		   home-desktop-shepherd-service-type)
-		  (service-extension
-                   home-profile-service-type
-                   home-desktop-profile-service)))
-                (default-value #f)))
+  (service-type
+   (name 'home-desktop)
+   (description "My desktop environment service.")
+   (extensions
+    (list
+     ;; Shepherd: supply dbus session bus
+     (service-extension
+      home-shepherd-service-type
+      home-desktop-shepherd-services)
+     ;; Profile packages
+     (service-extension
+      home-profile-service-type
+      home-desktop-profile-service)
+     ;; PipeWire + pipewire-pulse + wireplumber
+     (service-extension
+      home-pipewire-service-type
+      home-desktop-pipewire-service)))
+   (default-value #f)))
