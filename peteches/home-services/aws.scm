473626bc9b43db6edf2dc589effc3a9d62a56cee
Pete 'Peteches' McCabe <pete@peteches.co.uk>
2025-09-12 20:12:22 +0100

    Add PASS_PREFIX to aws home service

--- peteches/home-services/aws.scm
+++ peteches/home-services/aws.scm
@@ -1,17 +1,39 @@
 (define-module (peteches home-services aws)
   #:use-module (gnu home services)
+  #:use-module (gnu services configuration)
   #:use-module (gnu packages python-web)
-  #:use-module (gnu packages password-utils))
+  #:use-module (gnu packages password-utils)
+  #:export (home-aws-configuration))
+
+(define (serialize-string field val)
+  (cond
+   ((string? val) val)
+   ((symbol? val) (symbol->string val))))
 
 (define (aws-profile config)
   (list awscli aws-vault))
 
+(define-configuration home-aws-configuration
+  (vault-pass-prefix
+   (string "aws-vault")
+   "The AWS_VAULT_PASS_PREFIX to set. default aws-vault")
+  (vault-backend
+   (string "pass")
+   "The AWS_VAULT_BACKEND to set. default pass"))
+
+(define (home-aws-environment-variables-service-type config)
+  `(("AWS_VAULT_BACKEND" . ,(home-aws-configuration-vault-backend config))
+    ("AWS_VAULT_PASS_PREFIX" . ,(home-aws-configuration-vault-pass-prefix config))))
+
 (define-public home-aws-service-type
   (service-type (name 'aws)
 		(description "My aws setup")
-		(default-value '())
+		(default-value (home-aws-configuration))
 		(extensions
 		 (list
+		  (service-extension
+		   home-environment-variables-service-type
+		   home-aws-environment-variables-service-type)
 		  (service-extension
 		   home-profile-service-type
 		   aws-profile)))))
