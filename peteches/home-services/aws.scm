460469696dfd2710c0ec52abdbfc024d208354ce
Pete 'Peteches' McCabe <pete@peteches.co.uk>
2025-09-13 09:32:04 +0100

    Update aws service with aws-session-manager

--- peteches/home-services/aws.scm
+++ peteches/home-services/aws.scm
@@ -1,17 +1,21 @@
 (define-module (peteches home-services aws)
   #:use-module (gnu home services)
   #:use-module (gnu services configuration)
+  #:use-module (guix packages)
   #:use-module (gnu packages python-web)
   #:use-module (gnu packages password-utils)
-  #:export (home-aws-configuration))
+  #:use-module (peteches packages aws)
+  #:export (home-aws-configuration
+            home-aws-service-type))
+
+;; Serialize a boolean to "true"/"false"
+(define (serialize-boolean field val)
+  (if val "true" "false"))
 
 (define (serialize-string field val)
   (cond
    ((string? val) val)
    ((symbol? val) (symbol->string val))))
-
-(define (aws-profile config)
-  (list awscli aws-vault))
 
 (define-configuration home-aws-configuration
   (vault-pass-prefix
@@ -19,7 +23,20 @@
    "The AWS_VAULT_PASS_PREFIX to set. default aws-vault")
   (vault-backend
    (string "pass")
-   "The AWS_VAULT_BACKEND to set. default pass"))
+   "The AWS_VAULT_BACKEND to set. default pass")
+  (install-session-manager?
+   (boolean #t)
+   "If true, install the AWS Session Manager Plugin package.")
+  ;; Let callers override the package if they want a fork/custom build.
+  (session-manager-package
+   (package aws-session-manager-plugin)
+   "Package providing the 'session-manager-plugin' binary."))
+
+(define (aws-profile config)
+  (let ((base (list awscli aws-vault)))
+    (if (home-aws-configuration-install-session-manager? config)
+        (append base (list (home-aws-configuration-session-manager-package config)))
+        base)))
 
 (define (home-aws-environment-variables-service-type config)
   `(("AWS_VAULT_BACKEND" . ,(home-aws-configuration-vault-backend config))
---
+++ peteches/packages/aws.scm
@@ -0,0 +1,76 @@
+(define-module (peteches packages aws)
+  #:use-module (guix packages)
+  #:use-module (guix licenses)
+  #:use-module (guix gexp)                 ; for #~ gexps
+  #:use-module (guix download)
+  #:use-module (guix build-system gnu)
+  #:use-module (gnu packages golang))      ; for `go`
+
+(define-public aws-session-manager-plugin
+  (let* ((version "1.2.707.0")
+         (tarball (string-append
+                   "https://github.com/aws/session-manager-plugin/archive/refs/tags/"
+                   version ".tar.gz")))
+    (package
+      (name "aws-session-manager-plugin")
+      (version version)
+      (source
+       (origin
+         (method url-fetch)
+         (uri tarball)
+         ;; Use your verified base32:
+         (sha256 (base32 "16rql1vc25a6m0bzrhj69hrwpywfn1i1wmdrpnx244wb5wax38gp"))))
+      (build-system gnu-build-system)
+      (arguments
+       (list
+        #:tests? #f
+        #:phases
+        #~(modify-phases %standard-phases
+            (delete 'configure)
+            (replace 'build
+              (lambda _
+                (use-modules (guix build utils))
+                (let* ((pwd (getcwd))
+                       (cache (string-append pwd "/.cache/go-build"))
+                       (gopath (string-append pwd "/gopath"))
+                       (ghroot (string-append gopath "/src/github.com/aws"))
+                       (proj (string-append ghroot "/session-manager-plugin"))
+                       (vendorg (string-append proj "/vendor")))
+                  ;; Fix Go cache/tmp in sandbox:
+                  (setenv "HOME" pwd)
+                  (setenv "XDG_CACHE_HOME" (string-append pwd "/.cache"))
+                  (mkdir-p (getenv "XDG_CACHE_HOME"))
+                  (setenv "GOCACHE" cache)
+                  (mkdir-p cache)
+                  (setenv "GOTMPDIR" (string-append pwd "/.gotmp"))
+                  (mkdir-p (getenv "GOTMPDIR"))
+                  ;; Build in GOPATH mode with vendored deps:
+                  (setenv "GO111MODULE" "off")
+                  (mkdir-p ghroot)
+                  (symlink pwd proj)
+                  (setenv "GOPATH" (string-append gopath ":" vendorg))
+                  (with-directory-excursion proj
+                    (invoke "go" "build" "-o" "session-manager-plugin"
+                            "./src/sessionmanagerplugin/session")))))
+            (replace 'install
+              (lambda _
+                (use-modules (guix build utils))
+                (let* ((out   #$output)
+                       (bin   (string-append out "/bin"))
+                       (share (string-append out "/share/sessionmanagerplugin")))
+                  (mkdir-p bin)
+                  (mkdir-p share)
+                  (install-file
+                   "gopath/src/github.com/aws/session-manager-plugin/session-manager-plugin"
+                   bin)
+                  (when (file-exists? "VERSION")
+                    (install-file "VERSION" share))
+                  (when (file-exists? "seelog_unix.xml")
+                    (install-file "seelog_unix.xml" share))))))))
+      (supported-systems '("x86_64-linux" "aarch64-linux"))
+      (native-inputs (list go))
+      (home-page "https://github.com/aws/session-manager-plugin")
+      (synopsis "AWS Session Manager Plugin (built from source)")
+      (description "Client plugin the AWS CLI invokes for Systems Manager sessions, including port forwarding.")
+      (license asl2.0))))
+
