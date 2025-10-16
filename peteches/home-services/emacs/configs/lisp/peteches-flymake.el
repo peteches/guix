36205e441e103ba6fdf536e48c8caeff3e1c47ff
Pete 'Peteches' McCabe <pete@peteches.co.uk>
2025-10-16 21:11:16 +0100

    fix(flymake): fix flymake configs.

--- peteches/home-services/emacs/configs/lisp/peteches-flymake.el
+++ peteches/home-services/emacs/configs/lisp/peteches-flymake.el
@@ -1,8 +1,13 @@
-;; Generate a simple keymap for navigating errors with flymake
+;;; peteches-flymake --- configuring flymake
+;;; Commentary:
 
-
+;;; Code:
+(setq
+ flymake-show-diagnostics-at-end-of-line t)
+(flymake-mode)
 (define-key flymake-mode-map (kbd "M-n") 'flymake-goto-next-error)
 (define-key flymake-mode-map (kbd "M-p") 'flymake-goto-prev-error)
 
 
 (provide 'peteches-flymake)
+;;; peteches-flymake.el ends here
