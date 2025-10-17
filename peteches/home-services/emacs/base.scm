f35ca2493dc4c7bd406da24405eccf65690d127e
Pete 'Peteches' McCabe <pete@peteches.co.uk>
2025-10-17 18:58:47 +0100

    feat: Add Emacs Org-Mode Go support

--- peteches/home-services/emacs/base.scm
+++ peteches/home-services/emacs/base.scm
@@ -27,6 +27,7 @@
 
 (define (home-emacs-base-profile-service config)
   (map specification->package '("emacs"
+				"emacs-ob-go"
 				"emacs-modus-themes"
 				"emacs-gruvbox-theme"
 				"emacs-paredit"
