bb606cdce158866f39e86ff607e4d91f6f5da805
Pete 'Peteches' McCabe <pete@peteches.co.uk>
2025-10-17 18:57:11 +0100

    refactor: Move abbreviations to peteches-scoreplay and update commentary

--- peteches/home-services/emacs/configs/lisp/peteches-abbrev.el
+++ peteches/home-services/emacs/configs/lisp/peteches-abbrev.el
@@ -1,49 +1,15 @@
-(provide 'peteches-abbrev)
+;;; peteches-abbrev --- my abbrevs
+;;; Commentary:
+
+;;; Code:
 (require 'abbrev)
 
 (dolist (hook '(prog-mode-hook
 		text-mode-hook))
   (add-hook hook #'abbrev-mode))
 
-(dolist (abbrevs '(( "sp" . "ScorePlay")
-		   ( "ha" . "hybrid-agent")
-
-		   ( "agentPing" . "PingHealth" )
-		   ( "agentPong" . "PongHealth" )
-
-		   ( "TConfig" . "TestConfig" )
-		   ( "RConfig" . "RequestConfig" )
-
-		   ( "AddedFile" . "AddedFileNotification" )
-		   ( "DoneWriting" . "DoneWritingFileNotification" )
-		   ( "ChangedFile" . "ChangedFileNotification" )
-		   ( "RemovedFile" . "RemovedFileNotification" )
-
-		   ( "CreateVideo" . "CreateVideoProxyTask" )
-		   ( "CreateVideoP" . "CreateVideoProxyTaskProgressNotification" )
-		   ( "CreateVideoC" . "CreateVideoProxyTaskCompleteNotification" )
-
-		   ( "CreateLive" . "CreateLiveStreamProxyTask" )
-		   ( "CreateLiveP" . "CreateLiveStreamProxyTaskProgressNotification" )
-		   ( "CreateLiveC" . "CreateLiveStreamProxyTaskCompleteNotification" )
-
-		   ( "UploadTask" . "UploadToCloudTask" )
-		   ( "UploadTaskP" . "UploadToCloudTaskProgressNotification" )
-		   ( "UploadTaskC" . "UploadToCloudTaskCompleteNotification" )
-
-		   ( "DownloadTask" . "DownloadFromCloudTask" )
-		   ( "DownloadTaskP" . "DownloadFromCloudTaskProgressNotification" )
-		   ( "DownloadTaskC" . "DownloadFromCloudTaskCompleteNotification" )
-
-		   ( "ClipLiveTask" . "ClipLiveStreamTask" )
-		   ( "ClipLiveTaskP" . "ClipLiveStreamTaskProgressNotification" )
-		   ( "ClipLiveTaskC" . "ClipLiveStreamTaskCompleteNotification" )
-
-		   ( "ClipVideoTask" . "ClipVideoTask" )
-		   ( "ClipVideoTaskP" . "ClipVideoTaskProgressNotification" )
-		   ( "ClipVideoTaskC" . "ClipVideoTaskCompleteNotification" )
-
-		   ( "DeliverTask" . "DeliverToUserTask" )
-		   ( "DeliverTaskP" . "DeliverToUserTaskProgressNotification" )
-		   ( "DeliverTaskC" . "DeliverToUserTaskCompleteNotification" )))
+;;; global abbrevs
+(dolist (abbrevs '())
   (define-abbrev global-abbrev-table (car abbrevs) (cdr abbrevs)))
+(provide 'peteches-abbrev)
+;;; peteches-abbrev.el ends here.
--- peteches/home-services/emacs/configs/lisp/peteches-scoreplay.el
+++ peteches/home-services/emacs/configs/lisp/peteches-scoreplay.el
@@ -1,3 +1,11 @@
+;;; peteches-scoreplay --- My work related configs
+;;; Commentary:
+;;;
+;;; 1. Add Org-Capture templates
+;;; 2. Gnus email config
+;;; 3. Abreviations
+
+;;; Code:
 (require 'gnus)
 (require 'gnus-msg)
 (require 'message)
@@ -22,4 +30,48 @@
 	       (organization "ScorePlay Inc")
 	       ("X-Message-SMTP-Method"  "smtp smtp.gmail.com 587 peter.mccabe@scoreplay.io")))
 
+(dolist (abbrevs '(( "sp" . "ScorePlay")
+		   ( "ha" . "hybrid-agent")
+
+		   ( "agentPing" . "PingHealth" )
+		   ( "agentPong" . "PongHealth" )
+
+		   ( "TConfig" . "TestConfig" )
+		   ( "RConfig" . "RequestConfig" )
+
+		   ( "AddedFile" . "AddedFileNotification" )
+		   ( "DoneWriting" . "DoneWritingFileNotification" )
+		   ( "ChangedFile" . "ChangedFileNotification" )
+		   ( "RemovedFile" . "RemovedFileNotification" )
+
+		   ( "CreateVideo" . "CreateVideoProxyTask" )
+		   ( "CreateVideoP" . "CreateVideoProxyTaskProgressNotification" )
+		   ( "CreateVideoC" . "CreateVideoProxyTaskCompleteNotification" )
+
+		   ( "CreateLive" . "CreateLiveStreamProxyTask" )
+		   ( "CreateLiveP" . "CreateLiveStreamProxyTaskProgressNotification" )
+		   ( "CreateLiveC" . "CreateLiveStreamProxyTaskCompleteNotification" )
+
+		   ( "UploadTask" . "UploadToCloudTask" )
+		   ( "UploadTaskP" . "UploadToCloudTaskProgressNotification" )
+		   ( "UploadTaskC" . "UploadToCloudTaskCompleteNotification" )
+
+		   ( "DownloadTask" . "DownloadFromCloudTask" )
+		   ( "DownloadTaskP" . "DownloadFromCloudTaskProgressNotification" )
+		   ( "DownloadTaskC" . "DownloadFromCloudTaskCompleteNotification" )
+
+		   ( "ClipLiveTask" . "ClipLiveStreamTask" )
+		   ( "ClipLiveTaskP" . "ClipLiveStreamTaskProgressNotification" )
+		   ( "ClipLiveTaskC" . "ClipLiveStreamTaskCompleteNotification" )
+
+		   ( "ClipVideoTask" . "ClipVideoTask" )
+		   ( "ClipVideoTaskP" . "ClipVideoTaskProgressNotification" )
+		   ( "ClipVideoTaskC" . "ClipVideoTaskCompleteNotification" )
+
+		   ( "DeliverTask" . "DeliverToUserTask" )
+		   ( "DeliverTaskP" . "DeliverToUserTaskProgressNotification" )
+		   ( "DeliverTaskC" . "DeliverToUserTaskCompleteNotification" )))
+  (define-abbrev global-abbrev-table (car abbrevs) (cdr abbrevs)))
+
 (provide 'peteches-scoreplay)
+;;; peteches-scoreplay.el ends here
