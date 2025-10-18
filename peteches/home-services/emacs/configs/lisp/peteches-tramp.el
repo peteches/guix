0716967b560f9db32dbeb8dbbd3aed0329b01254
Pete 'Peteches' McCabe <pete@peteches.co.uk>
2025-10-18 20:55:05 +0100

    feat(peteches-tramp.el): Add remote path configuration and improve Commentary section

--- peteches/home-services/emacs/configs/lisp/peteches-tramp.el
+++ peteches/home-services/emacs/configs/lisp/peteches-tramp.el
@@ -1,7 +1,22 @@
-(provide 'peteches-tramp)
+;;; peteches-tramp --- my tramp setup
+
+;;; Commentary:
+
+
+;;; Code:
 (require 'tramp)
 (message "loading tramp config")
 
+(setq tramp-remote-path
+      (append tramp-remote-path
+              (list
+	       "tramp-own-remote-path"
+	       "/run/privileged/bin"
+               "~/.config/guix/current/bin"
+               "~/.guix-home/profile/bin"
+               "~/.guix-home/profile/sbin"
+               "/run/current-system/profile/bin"
+               "/run/current-system/profile/sbin")))
 
 ;; TRAMP does not use default ssh ControlPath
 ;; TRAMP overwrites ControlPath settings when initiating ssh sessions. TRAMP does this to fend off a stall if a master session opened outside the Emacs session is no longer open. That is why TRAMP prompts for the password again even if there is an ssh already open.
@@ -9,5 +24,8 @@
 
 (setq tramp-ssh-controlmaster-options
       (concat
-        "-o ControlPath=/tmp/ssh-ControlPath-%%r@%%h:%%p "
-        "-o ControlMaster=auto -o ControlPersist=yes"))
+       "-o ControlPath=/tmp/ssh-ControlPath-%%r@%%h:%%p "
+       "-o ControlMaster=auto -o ControlPersist=yes"))
+
+(provide 'peteches-tramp)
+;;; peteches-tramp.el ends here
