a91bd7aa487e0eeb946deb948222e06c17157590
Pete 'Peteches' McCabe <pete@peteches.co.uk>
2025-10-29 20:29:03 +0000

    Disable linear MCP until later when it might be useful.

--- peteches/home-services/emacs/configs/lisp/peteches-mcp.el
+++ peteches/home-services/emacs/configs/lisp/peteches-mcp.el
@@ -23,16 +23,17 @@
 	("filesystem-primary-work" . (:command "mcp-filesystem-server" :args ("/home/peteches/area_51/github.com/ScorePlay-Inc/backend-monorepo.git/checkouts/primaryWork" )))
 	("filesystem-secondary-work" . (:command "mcp-filesystem-server" :args ("/home/peteches/area_51/github.com/ScorePlay-Inc/backend-monorepo.git/checkouts/secondaryWork" )))
 	("shell" . (:command "mcp-shell"))
-	("linear" . (:command "docker" :args (
-					      "run"
-;					      "--rm"
-					      "--interactive"
-					      "--net=host"
-					      "--user" ,(number-to-string (user-uid))
-					      "--volume" ,(concat (expand-file-name "~/.mcp-remote-go-auth") ":/.mcp-remote-go-auth")
-					      "--publish" "3334:3334"
-					      "ghcr.io/naotama2002/mcp-remote-go:latest"
-					      "https://mcp.linear.app/sse")))))
+;; 	("linear" . (:command "docker" :args (
+;; 					      "run"
+;; ;					      "--rm"
+;; 					      "--interactive"
+;; 					      "--net=host"
+;; 					      "--user" ,(number-to-string (user-uid))
+;; 					      "--volume" ,(concat (expand-file-name "~/.mcp-remote-go-auth") ":/.mcp-remote-go-auth")
+;; 					      "--publish" "3334:3334"
+;; 					      "ghcr.io/naotama2002/mcp-remote-go:latest"
+;; 					      "https://mcp.linear.app/sse")))
+	))
 
 ;; Start hub
 (add-hook 'after-init-hook
