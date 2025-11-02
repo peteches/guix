568f664519895cd19f4e17867cfbb54b50f4fc51
Pete 'Peteches' McCabe <pete@peteches.co.uk>
2025-11-02 15:11:50 +0000

    feat: Add snippet directory management and Magit interaction
    This commit adds functionality to manage the yasnippet directory by cloning it if it doesn't exist and provides a Magit status command for interacting with the snippet directory.

--- peteches/home-services/emacs/configs/lisp/peteches-yasnippet.el
+++ peteches/home-services/emacs/configs/lisp/peteches-yasnippet.el
@@ -6,6 +6,15 @@
 (require 'yasnippet)
 (yas-global-mode 1)
 
+(setq peteches/snippet-dir (expand-file-name "~/.config/emacs/snippets"))
+
+(if (not (f-directory? peteches/snippet-dir))
+    (magit-git "clone" "git@github.com:peteches/yasnippets.git" peteches/snippet-dir))
+
+(defun peteches/snippit-git ()
+    "Open magit status for the snippet dir."
+    (interactive)
+    (magit-status "~/.config/emacs/snippets"))
 
 (provide `peteches-yasnippet)
 ;;; peteches-yasnippet.el ends here
