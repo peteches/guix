0482d80803ffed06eb65cae8040b98dcd477de02
Pete 'Peteches' McCabe <pete@peteches.co.uk>
2025-11-03 09:41:32 +0000

    feat: add emacs-forge

--- peteches/home-services/emacs/base.scm
+++ peteches/home-services/emacs/base.scm
@@ -27,6 +27,7 @@
 
 (define (home-emacs-base-profile-service config)
   (map specification->package '("emacs"
+				"emacs-forge"
 				"emacs-string-inflection"
 				"emacs-ob-go"
 				"emacs-modus-themes"
