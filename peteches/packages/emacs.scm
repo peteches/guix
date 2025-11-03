5d82dec5ed97aa35e46c80271fc6712805091ed2
Pete 'Peteches' McCabe <pete@peteches.co.uk>
2025-11-03 09:41:59 +0000

    feat: update emacs-slack package

--- peteches/packages/emacs.scm
+++ peteches/packages/emacs.scm
@@ -1,31 +1,49 @@
 (define-module (peteches packages emacs)
   #:use-module (gnu packages emacs)
-  #:use-module (gnu packages emacs-xyz)
+  #:use-module ((gnu packages emacs-xyz)
+		#:renamer (symbol-prefix-proc 'upstream:))
   #:use-module (guix git-download)
   #:use-module (guix build-system emacs)
   #:use-module (guix licenses)
   #:use-module (guix packages))
 
-(define-public emacs-mcp
-  (package
-    (name "emacs-mcp")
-    (version "0.1.0")
-    (source (origin
+(define-public emacs-slack
+  ;; Update the commit and revision
+  (let ((commit "4c34c526a7f5fa51a78bf21c0d3420f74d79df58")          ; ← replace with the new commit
+        (revision "12"))                    ; increment the revision
+    (package
+     (inherit upstream:emacs-slack)
+     (version (git-version "0.0.2" revision commit))
+     (source (origin
               (method git-fetch)
               (uri (git-reference
-                    (url "https://github.com/lizqwerscott/mcp.el")
-                    (commit "2fcda27b15395fe1ed55d15a88e6187a69af09e9")))
-              (file-name (git-file-name name version))
+                    (url "https://github.com/yuya373/emacs-slack")
+                    (commit commit)))
+              (file-name (git-file-name "emacs-slack" commit))
               (sha256
                (base32
-                "0q4ycgb4dx4p0v449vncmb4j0nklpy14wasz4x7pkyq6yhsx8lmz"))))
-    (build-system emacs-build-system)
-    (home-page "https://github.com/lizqwerscott/mcp.el")
-    (synopsis "Prompt manager for interacting with LLMs in Emacs")
-    (description
-     "mcp.el is an Emacs interface for managing reusable prompt templates and interacting with LLMs like ChatGPT via GPTel.")
-    (license gpl3+)))
-emacs-mcp
+                "1l7rkzpjfq4zzl4pwzlmmjmabb1rk04slygrplsis4r8rg9anjh4")))))))
+
+(define-public emacs-mcp
+  (package
+   (name "emacs-mcp")
+   (version "0.1.0")
+   (source (origin
+            (method git-fetch)
+            (uri (git-reference
+                  (url "https://github.com/lizqwerscott/mcp.el")
+                  (commit "2fcda27b15395fe1ed55d15a88e6187a69af09e9")))
+            (file-name (git-file-name name version))
+            (sha256
+             (base32
+              "0q4ycgb4dx4p0v449vncmb4j0nklpy14wasz4x7pkyq6yhsx8lmz"))))
+   (build-system emacs-build-system)
+   (home-page "https://github.com/lizqwerscott/mcp.el")
+   (synopsis "Prompt manager for interacting with LLMs in Emacs")
+   (description
+    "mcp.el is an Emacs interface for managing reusable prompt templates and interacting with LLMs like ChatGPT via GPTel.")
+   (license gpl3+)))
+emacs-slack
 
 ;; (define-public emacs-gotest
 ;;   (package
