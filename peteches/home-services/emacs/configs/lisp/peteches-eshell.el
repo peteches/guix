88ae6108e9592cb74719fce956a59fc6851224e1
Pete 'Peteches' McCabe <pete@peteches.co.uk>
2025-11-06 20:49:07 +0000

    feat(eshell): Update some shell vars
    - destroy term buffers and add btop to visual commands
    
    footer

--- peteches/home-services/emacs/configs/lisp/peteches-eshell.el
+++ peteches/home-services/emacs/configs/lisp/peteches-eshell.el
@@ -1,5 +1,16 @@
-(provide 'peteches-eshell)
+;;; peteches-eshell --- my eshell configs
+;;;
+;;; Commentary:
+;;; 
+;;;
+;;; Code:
+
 (require 'eshell)
 (message "Loading eshell configs")
 (setq
+ eshell-destroy-buffer-when-process-dies t
  eshell-prefer-lisp-functions t)
+
+(add-to-list 'eshell-visual-commands "btop")
+(provide 'peteches-eshell)
+;;; peteches-eshell.el ends here
