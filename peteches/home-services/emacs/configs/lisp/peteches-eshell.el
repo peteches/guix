e028c7af33517857b27dd053112b4b4212d14aef
Pete 'Peteches' McCabe <pete@peteches.co.uk>
2025-11-10 23:21:13 +0000

    feat: Adding aweshell to make eshell awesome

--- peteches/home-services/emacs/configs/lisp/peteches-eshell.el
+++ peteches/home-services/emacs/configs/lisp/peteches-eshell.el
@@ -20,18 +20,13 @@
     "Make a better more interactive cd for eshell."
   (let ((subdirs (split-string
 		  (shell-command-to-string (format "find %s -type d " default-directory)) "\n")))
-    (setq default-directory (completing-read "Choose directory to cd: " subdirs 'stringp))))
+    (eshell/cd (completing-read "Choose directory to cd: " subdirs 'stringp))))
 
-
-(defun eshell/ucd ()
-  "Make traversing to parent directories easier."
-  (let* ((dir-components (split-string default-directory "/"))
-	 (dirs (let ((dir '()) (path "/"))
-		 (dolist (d dir-components)
-		   (setq path (f-join path d))
-		   (append dir path)))))
-    (setq default-directory (completing-read "Choose parent to cd: " dirs))))
+(use-package aweshell
+  :straight (aweshell :host github :repo "manateelazycat/aweshell")
+  :config (require 'eshell-up))
 
 (add-to-list 'eshell-visual-commands "btop")
+
 (provide 'peteches-eshell)
 ;;; peteches-eshell.el ends here
