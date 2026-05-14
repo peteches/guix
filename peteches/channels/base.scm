2bf0b1c3ab2152cc011980c055be6726577bc029
Pete 'Peteches' McCabe <pete@peteches.co.uk>
2026-05-14 17:34:24 +0100

    feat: add guix-science-nonfree channel
    - Added Guix-Science (non-free) channel to base.scm.
    - Removed Guix-Science (non-free) channel from nug.scm.

--- peteches/channels/base.scm
+++ peteches/channels/base.scm
@@ -25,6 +25,17 @@
        "b1fe5aaff3ab48e798a4cce02f0212bc91f423dc"
        (openpgp-fingerprint
         "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
+        ;; Guix-Science (non-free)
+    (channel
+     (name 'guix-science-nonfree)
+     (url "https://codeberg.org/guix-science/guix-science-nonfree.git")
+     (branch "master")
+     (commit "6d5025d4c68a6ce6abbf5189fad0f97cafd2bba1")
+     (introduction
+      (make-channel-introduction
+       "58661b110325fd5d9b40e6f0177cc486a615817e"
+       (openpgp-fingerprint
+        "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
     (channel
      (name 'nonguix)
      (url "https://gitlab.com/nonguix/nonguix")
--- peteches/channels/nug.scm
+++ peteches/channels/nug.scm
@@ -7,19 +7,6 @@
   (append
    %base-channels
    (list
-
-    ;; Guix-Science (non-free)
-    (channel
-     (name 'guix-science-nonfree)
-     (url "https://codeberg.org/guix-science/guix-science-nonfree.git")
-     (branch "master")
-     (commit "6d5025d4c68a6ce6abbf5189fad0f97cafd2bba1")
-     (introduction
-      (make-channel-introduction
-       "58661b110325fd5d9b40e6f0177cc486a615817e"
-       (openpgp-fingerprint
-        "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
-
     ;; (channel
     ;;  (name 'peteches)
     ;;  (url "file:///home/peteches/area_51/peteches-channel")
