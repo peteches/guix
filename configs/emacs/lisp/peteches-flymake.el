87b429b58e559f34d5c123475b997155b54216b0
Pete 'Peteches' McCabe <pete@peteches.co.uk>
2026-06-26 15:02:47 +0100

    feat: improve flymake ui

--- configs/emacs/lisp/peteches-flymake.el
+++ configs/emacs/lisp/peteches-flymake.el
@@ -1,20 +1,32 @@
-;;; peteches-flymake.el --- Configure Flymake -*- lexical-binding: t; -*-
-
+;;; peteches-flymake --- configuring flymake -*- lexical-binding: t; -*-
 ;;; Commentary:
 ;;;
-;;; Lightweight diagnostics UI.  `peteches-lsp' configures lsp-mode to publish
-;;; diagnostics through Flymake.
-
+;;; Flymake diagnostics should be visible, but not inserted visually at the
+;;; end of the current line while typing.
+;;;
 ;;; Code:
 
 (require 'flymake)
+(require 'eldoc)
 
-(setq flymake-show-diagnostics-at-end-of-line t)
+(setq flymake-show-diagnostics-at-end-of-line nil)
+
+(defun peteches/flymake-setup-eldoc ()
+  "Prefer Flymake diagnostics in Eldoc's echo-area output."
+  (eldoc-mode 1)
+  (when (boundp 'eldoc-documentation-functions)
+    (setq-local eldoc-documentation-functions
+                (cons #'flymake-eldoc-function
+                      (remove #'flymake-eldoc-function
+                              eldoc-documentation-functions)))))
 
 (add-hook 'prog-mode-hook #'flymake-mode)
+(add-hook 'flymake-mode-hook #'peteches/flymake-setup-eldoc)
 
 (define-key flymake-mode-map (kbd "M-n") #'flymake-goto-next-error)
 (define-key flymake-mode-map (kbd "M-p") #'flymake-goto-prev-error)
+(define-key flymake-mode-map (kbd "C-c ! b") #'flymake-show-buffer-diagnostics)
+(define-key flymake-mode-map (kbd "C-c ! p") #'flymake-show-project-diagnostics)
 
 (provide 'peteches-flymake)
 ;;; peteches-flymake.el ends here
