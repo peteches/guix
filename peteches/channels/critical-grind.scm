d44f3449effdc874cee7fc8cb460876bb639ff1b
concourse-ci <concourse-ci@critical-grind-campaign>
2026-09-13 08:39:19 +0000

    critical-grind: bump channel to f8beec1244e3ef92ebb88afd4462f0ca12c33240 (automated, gate pipeline)

--- peteches/channels/critical-grind.scm
+++ peteches/channels/critical-grind.scm
@@ -25,4 +25,4 @@
   (name 'critical-grind)
   (url "git@github.com:peteches/critical-grind-battlefronts.git")
   (branch "main")
-  (commit "f31e1f0fe1572347a1285e448dd07388e54b4c4a")))
+  (commit "f8beec1244e3ef92ebb88afd4462f0ca12c33240")))
